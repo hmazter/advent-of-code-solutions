2a742cfad58943cea1334d0b5fa6ffb61f73215d
Kristoffer Högberg <krihog@gmail.com>
2015-12-10 08:33:36 +0100

    Cleanup of day 10

--- day10/day10.php
+++ day10/day10.php
@@ -1,24 +1,16 @@
 <?php
 
 $input = '1113222113';
-
-// examples
-//$input = '1';
-//$input = '11';
-//$input = '21';
-//$input = '1211';
-//$input = '111221';
-
 $loopTimes = 50;
+
 for ($i = 0; $i < $loopTimes; $i++) {
     $chars = str_split($input);
     $output = [];
     $index = 0;
     $previous = null;
-    $count = 0;
 
     foreach ($chars as $char) {
-        if ($previous != $char) {
+        if ($char != $previous) {
             $index++;
             $output[$index] = ['char' => $char, 'count' => 0];
         }
