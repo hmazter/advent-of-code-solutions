ba6968f7222d3282956db53f2393e22e0b4c999c
Kristoffer Högberg <krihog@gmail.com>
2016-12-03 19:53:37 +0100

    Day 3, Sort lenghts and just summing the two shortest

--- day3/part1.php
+++ day3/part1.php
@@ -7,14 +7,10 @@ $rows = file($file);
 $validTriangles = 0;
 
 foreach ($rows as $row) {
-    $triangle = array_map('trim', str_split($row, 5));
+    $triangle = array_map('trim', str_split(trim($row, "\n"), 5));
     $triangle = array_map('trim', $triangle);
-
-    if (
-        ($triangle[0] + $triangle[1]) > $triangle[2] &&
-        ($triangle[0] + $triangle[2]) > $triangle[1] &&
-        ($triangle[1] + $triangle[2]) > $triangle[0]
-    ) {
+    sort($triangle);
+    if (($triangle[0] + $triangle[1]) > $triangle[2]) {
         $validTriangles++;
     }
 }
--- day3/part2.php
+++ day3/part2.php
@@ -19,11 +19,8 @@ while ($colIndex < 3) {
 
 $triangles = array_chunk($allValues, 3);
 foreach ($triangles as $triangle) {
-    if (
-        ($triangle[0] + $triangle[1]) > $triangle[2] &&
-        ($triangle[0] + $triangle[2]) > $triangle[1] &&
-        ($triangle[1] + $triangle[2]) > $triangle[0]
-    ) {
+    sort($triangle);
+    if (($triangle[0] + $triangle[1]) > $triangle[2]) {
         $validTriangles++;
     }
 }
