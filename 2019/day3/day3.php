ce631b477254a68270a8fde62a141defce5d18ad
Kristoffer Högberg <krihog@gmail.com>
2019-12-04 13:51:17 +0100

    2019 day 3 fix import path

--- 2019/day3/day3.php
+++ 2019/day3/day3.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/functions.php';
 
-$rows = readRows('input.txt');
+$rows = readRows(__DIR__ .'/input.txt');
 $path1 = explode(',', $rows[0]);
 $path2 = explode(',', $rows[1]);
 
