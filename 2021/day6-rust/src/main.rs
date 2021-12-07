4977fcb0056fb99fa645fe5bd1c968bff8f7783a
Kristoffer Högberg <krihog@gmail.com>
2021-12-07 21:01:57 +0100

    Reorganized for working solution

--- 2021/day6-rust/src/main.rs
+++ 2021/day6-rust/src/main.rs
@@ -1,28 +1,26 @@
-fn count_lanternfish(input: &[usize], days: i32) -> u64 {
-    let mut ages: [usize; 9] = [0; 9];
-
-    for age in input {
-        ages[age] += 1
-    }
-
+fn count_lanternfish(mut ages: [u64; 9], days: i32) -> u64 {
     for _i in 0..days {
         ages.rotate_left(1); // move age 0 to new fish as age 8
         ages[6] += ages[8]; // reset fish to age 6
     }
 
     // count all the fishes
-    return ages.iter().sum();
+    return ages.iter().sum::<u64>();
 }
 
-fn parse_input(input: &str) -> &[usize] {
-    return input
+fn group_ages(input: &str) -> [u64; 9] {
+    let mut ages: [u64; 9] = [0; 9];
+
+    input
         .split(",")
-        .map(|s| s.trim().parse::<usize>().unwrap())
-        .collect();
+        .map(|num| num.trim().parse::<usize>().unwrap())
+        .for_each(|age| ages[age] += 1);
+
+    return ages;
 }
 
 fn main() {
-    let input = parse_input(include_str!("../input"));
+    let input = group_ages(include_str!("../input"));
 
     println!("part 1: {}", count_lanternfish(input, 80));
     println!("part 2: {}", count_lanternfish(input, 256));
@@ -33,15 +31,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_input() {
-        let input = parse_input("3,4,3,1,2");
+    fn test_group_ages() {
+        let input = group_ages("3,4,3,1,2");
 
-        assert_eq!(input, [3, 4, 3, 1, 2]);
+        assert_eq!(input, [0, 1, 1, 2, 1, 0, 0, 0, 0]);
     }
 
     #[test]
     fn solve_part1() {
-        let input = parse_input("3,4,3,1,2");
+        let input = group_ages("3,4,3,1,2");
 
         assert_eq!(count_lanternfish(input, 18), 26);
         assert_eq!(count_lanternfish(input, 80), 5934);
@@ -49,7 +47,7 @@ mod tests {
 
     #[test]
     fn solve_part2() {
-        let input = parse_input("3,4,3,1,2");
+        let input = group_ages("3,4,3,1,2");
 
         assert_eq!(count_lanternfish(input, 256), 26984457539);
     }
