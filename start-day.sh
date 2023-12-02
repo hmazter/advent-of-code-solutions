f60a2b27e78a2d0dc77f27acc1bd9ad1801891e8
Kristoffer Högberg <krihog@gmail.com>
2023-12-02 21:26:56 +0100

    Fix start script

--- start-day.sh
+++ start-day.sh
@@ -5,7 +5,7 @@ day=$2
 mkdir -p $year/day$day
 
 cp dayExample/day.php "$year/day${day}/day${day}.php"
-cp dayExample/dayTest.php "$year/day${day}/day${day}Test.php"
+cp dayExample/DayTest.php "$year/day${day}/Day${day}Test.php"
 cp dayExample/functions.php "$year/day${day}/functions.php"
 
-sed -i '' "s/DayTest/Day${day}Test/g" "$year/day${day}/day${day}Test.php"
+sed -i '' "s/DayTest/Day${day}Test/g" "$year/day${day}/Day${day}Test.php"
