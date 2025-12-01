1ef9354f71a6f0bcec2726c7551219de6fb0cfce
Kristoffer Högberg <krihog@gmail.com>
2025-12-01 11:09:11 +0100

    update readme and year scripts

---
+++ 2025/2025.php
@@ -0,0 +1,10 @@
+<?php
+
+foreach (range(1, 12) as $day) {
+    $filename = __DIR__ . "/day{$day}/day{$day}.php";
+    if (file_exists($filename)) {
+        echo "\n=====================\n";
+        echo "Day $day\n";
+        system(PHP_BINARY . ' ' . $filename);
+    }
+}
